d081cc857ab0df52802245dfd167b0913b287364
alexis-82 <giobbia82@gmail.com>
2015-03-19 19:06:12 +0100

    new project

---
+++ Installazione.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Avvio installazione
+echo
+echo "*******************************"
+echo "* Installazione Startup Sound *"
+echo "*******************************"
+echo
+echo "Controllo e installazione delle librerie..."
+sleep 5
+sudo apt-get update
+sudo apt-get install mplayer
+sleep 3
+echo
+echo "Attendere un momento!"
+sleep 3
+echo
+echo "Creazione dello script..."
+echo
+
+# Creazione dello script
+mkdir /home/$USER/SuoniAvvio
+cp *.wav /home/$USER/SuoniAvvio
+cd /home/$USER/SuoniAvvio
+touch start
+echo "#!/bin/bash" >> start
+echo "sleep 0" >> start
+echo "StartupNotify=false" >> start
+echo "/usr/bin/mplayer /home/$USER/SuoniAvvio/sound6.wav" >> start
+chmod u+x start
+
+# Creazione lanciatore
+sleep 3
+echo "Creazione automatica del lanciatore..."
+cd /home/$USER/.config/autostart/
+touch avviosuono.desktop
+echo "[Desktop Entry]" >> avviosuono.desktop
+echo "Type=Application" >> avviosuono.desktop
+echo "Name=SuonoAvvio" >> avviosuono.desktop
+echo "Exec=/home/$USER/SuoniAvvio/start" >> avviosuono.desktop
+echo "Icon=system-run" >> avviosuono.desktop
+echo "Terminal=false" >> avviosuono.desktop
+echo "StartupNotify=false" >> avviosuono.desktop
+echo "X-GNOME-Autostart-enabled=true" >> avviosuono.desktop
+sleep 5
+echo
+echo "Operazione terminata! :)"
+echo
---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Installazione
+Aprire il terminale e lanciare il comando:
+
+./Installazione.sh
+
+# Come cambiare suono
+Aprire il file START e modificare la riga:
+
+/usr/bin/mplayer /home/$USER/SuoniAvvio/sound6.wav
+
+sostituire sound6.wav con un file.wav vostro, ricordarsi di copiare il
+file.wav nella cartella /SuoniAvvio
+
+By Alexis82 & Alessandro De Filippis
+
+YouTube: https://www.youtube.com/user/giobbia82
